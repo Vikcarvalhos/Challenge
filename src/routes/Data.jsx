8675ca43fe23a2fb7208bccd0b9197d85ca8bf21
Vikcarvalhos <viktorcarvalhos@gmail.com>
2024-03-31 02:18:48 -0300

    feat:prototype and data page concluded

--- src/routes/Data.jsx
+++ src/routes/Data.jsx
@@ -1,9 +1,27 @@
+import React from 'react'
+import '../css/data.css'
+import map from '../assets/img/data/1.svg'
+
 function Data(){
 
     return(
         <>
-            <main id='home-main'>
-                
+            <main id='data'>
+                <section className='data-content'>
+                    <div className='data-text'>
+                        <div className='data-title'>
+                            <h1>Problem</h1>
+                            <h2>Data</h2>
+                        </div>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Laboriosam quod porro recusandae dolores optio. 
+                            Iusto ipsa porro animi optio? Nemo ducimus dolore ipsa cumque quis iusto adipisci nam veniam ipsam.
+                            </p>
+                        <button className='data-btn'>Research</button>
+                    </div>
+                    <img src={map} alt='map' className='data-img'/>
+                </section>
             </main>
         </>
     )
--- src/routes/Prototype.jsx
+++ src/routes/Prototype.jsx
@@ -1,9 +1,31 @@
+import React from 'react'
+import '../css/prototype.css'
+import rectImg from '../assets/img/prototype/1.svg'
+import sqrImg from '../assets/img/prototype/2.svg'
+
 function Prototype(){
 
     return(
         <>
-            <main id='home-main'>
-                
+            <main id='prototype'>
+                <h1>Problem</h1>
+                <h2>Data</h2>
+                <section className='prototype-content'>
+                    <img src={rectImg} alt='rectangle' className='prototype-img'/>
+                    <div className='prototype-text'>
+                        <img src={sqrImg} alt='square' className='prototype-img'/>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quod porro recusandae dolores optio. 
+                            Iusto ipsa porro animi optio? Nemo ducimus dolore ipsa cumque quis iusto adipisci nam veniam ipsam.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum blanditiis placeat voluptates distinctio accusamus 
+                            fuga nemo adipisci! Eius maiores soluta facere quae laudantium, exercitationem, illo saepe praesentium, 
+                            doloremque asperiores ipsam!
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora repellendus totam quae provident distinctio unde, 
+                            reiciendis qui atque nulla modi vero recusandae, fugit omnis placeat quisquam doloribus ipsam consectetur. Rerum.
+                        </p>
+                    </div>
+                    <img src={rectImg} alt='rectangle' className='prototype-img'/>
+                </section>
             </main>
         </>
     )
