9236abcfe96371933045008c702814b897a99f26
Vikcarvalhos <viktorcarvalhos@gmail.com>
2024-03-31 01:44:59 -0300

    feat: concept and how it works pages completed

--- src/routes/Concept.jsx
+++ src/routes/Concept.jsx
@@ -1,9 +1,37 @@
+import React from 'react'
+import '../css/concept.css'
+import imgNull from '../assets/img/concept/image.svg'
+
 function Concept(){
 
     return(
         <>
-            <main id='home-main'>
-                
+            <main id='concept'>
+                <h1>Project</h1>
+                <h2>Concepts</h2>
+                <section className='concept-container'>
+                    <div className='concept-div'>
+                        <img src={imgNull} alt='image' />
+                        <div className='concept-text'>
+                            <h3>1</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam</p>
+                        </div>
+                    </div>
+                    <div className='concept-div-inverted'>
+                        <img src={imgNull} alt='image' />
+                        <div className='concept-text'>
+                            <h3>2</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam</p>
+                        </div>
+                    </div>
+                    <div className='concept-div'>
+                        <img src={imgNull} alt='image' />
+                        <div className='concept-text'>
+                            <h3>3</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam</p>
+                        </div>
+                    </div>
+                </section>
             </main>
         </>
     )
--- src/routes/Howitworks.jsx
+++ src/routes/Howitworks.jsx
@@ -1,9 +1,41 @@
+import React from 'react'
+import '../css/howitworks.css'
+import imgNull from '../assets/img/howitworks/image.svg'
+import { GoArrowRight } from "react-icons/go";
+
 function Howitworks(){
 
     return(
         <>
-            <main id='home-main'>
-                
+            <main id='how-it-works'>
+                <h1>How it</h1>
+                <h2>Works</h2>
+                <section className='work-container'>
+                    <div className='work-method'>
+                        <img src={imgNull} alt='image' />
+                        <div className='work-text'>
+                            <h3>Step 1</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, ducimus iste delectus reprehenderit officia tempore repellat, nam iusto nesciunt sit quasi eaque! Laboriosam illo at eaque esse suscipit earum sunt!</p>
+                            <button className='work-btn'>View More <span><GoArrowRight /></span></button>
+                        </div>
+                    </div>
+                    <div className='work-method'>
+                        <img src={imgNull} alt='image' />
+                        <div className='work-text'>
+                            <h3>Step 2</h3>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam rerum dignissimos corporis explicabo, velit dolorem ut odit sunt autem placeat itaque saepe maxime. Impedit sequi nobis soluta, eos modi eligendi?</p>
+                            <button className='work-btn'>View More <span><GoArrowRight /></span></button>
+                        </div>
+                    </div>
+                    <div className='work-method'>
+                        <img src={imgNull} alt='image' />
+                        <div className='work-text'>
+                            <h3>Step 3</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, sunt animi? Eligendi error mollitia beatae, reprehenderit tempora laborum ullam odio, blanditiis perferendis aut quasi numquam dignissimos, hic enim. Corporis, id!</p>
+                            <button className='work-btn'>View More <span><GoArrowRight /></span></button>
+                        </div>
+                    </div>
+                </section>
             </main>
         </>
     )
