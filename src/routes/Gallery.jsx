e257f37c5363cb40c3cb93f8b234011c8d6a150d
Vikcarvalhos <viktorcarvalhos@gmail.com>
2024-03-30 23:07:44 -0300

    feat: gallery finished

--- src/routes/Gallery.jsx
+++ src/routes/Gallery.jsx
@@ -1,13 +1,25 @@
+import React from 'react';
+import '../css/gallery.css';
+import image1 from '../assets/img/gallery/1.svg';
+import image2 from '../assets/img/gallery/2.svg';
+import image3 from '../assets/img/gallery/3.svg';
+import image4 from '../assets/img/gallery/4.svg';
+import image5 from '../assets/img/gallery/5.svg';
 
-function Gallery(){
+function Gallery() {
+  const images = [image1, image2, image3, image4, image5];
 
-    return(
-        <>
-            <main id='home-main'>
-                
-            </main>
-        </>
-    )
+  return (
+    <main className='gallery'>
+        <h1>Photo</h1>
+        <h2>Gallery</h2>
+        <div className='gallery-images'>
+            {images.map((image, index) => (
+            <img key={index} src={image} alt={`Image ${index + 1}`} />
+            ))}
+        </div>
+    </main>
+  );
 }
 
-export default Gallery
+export default Gallery;
